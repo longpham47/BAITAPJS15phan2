33342ec3b0e240fa10f1ac76ebac9df781adbca4
Plong <phamlonganhdao@gmail.com>
2022-07-16 01:07:06 +0700

    bai2

---
+++ BAI2/bai2.js
@@ -0,0 +1,53 @@
+
+
+
+function tinhTien() {
+    var loaiKhachHang = document.getElementById("khachHang").value;
+    var maKH = Number(document.getElementById("maKH").value);
+    var soKenh = Number(document.getElementById("soKenh").value);
+    var soKN = Number(document.getElementById("soKN").value);
+
+    var tiencap = 0;
+   
+   
+    switch (loaiKhachHang) {
+        case "DNnghiep":
+            if (soKN < 10) {
+                tiencap = 15 + 75 + 50 * soKenh;;
+            } else {
+                tiencap = 15 + 75 + 50 * soKenh + (soKN - 10) * 5;
+            }
+
+            break;
+        
+        case "NDan":
+            
+
+            tiencap = 4.5 + 20.5 + (7.5 * soKenh);
+            document.getElementById("anHien").style.display = "none"
+            
+         
+            break;
+
+        default:
+            alert("hãy chọn loại khách hàng");
+            break;
+            
+    }
+    
+    document.getElementById("txtResult").innerHTML ="Mã khách hàng:" + maKH +"; Tiền cáp: "+" $" + tiencap.toLocaleString();
+
+}
+function anHien(){
+    var anhien = document.getElementById("khachHang").value;
+    document.getElementById("anHien").style.display = ""
+    if(anhien == "NDan"){
+         "none"
+
+    }else{
+         "block"
+    }
+    
+}
+
+document.getElementById("btnT").onclick = tinhTien;
